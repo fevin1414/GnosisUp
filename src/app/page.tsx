c9f528546ab130706b5daa50770ccc8cf2675c21
fevin1414 <fevinbiju1414@gmail.com>
2025-05-21 19:07:28 -0500

    Feat : Fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import FontDemo from "@/UI/FontDemo";
-import Image from "next/image";
-import DashboardLayout from "./(dashboard)/dashboard/layout";
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <DashboardLayout />
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen gap-6">
+      <h1 className="text-3xl font-bold">Welcome to GnosisUp</h1>
+      <p>
+        This is the Landing page signIn/SignUp other website part will be added
+        to here now since no authentiation is done clicking the button would
+        take us to dashboard
+      </p>
+      <Link href="/dashboard">
+        <Button>Go to Dashboard</Button>
+      </Link>
+    </main>
   );
 }
